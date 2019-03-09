ba6a3624c483e12ecb99dd0bec950f7e177087c6
F John De Costa <john@mayerfleur.com>
2019-03-09 16:55:39 +1100

    Removed unneeded files

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,34 +1,35 @@
 module.exports = {
-  siteMetadata: {
-    title: `FCC Calculator`,
-    description: `Freecodecamp project`,
-    author: `F. John De Costa`,
-  },
-  plugins: [
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/fox-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // 'gatsby-plugin-offline',
-  ],
+	siteMetadata: {
+		title: "FCC Calculator",
+		description: "Freecodecamp project",
+		author: "F. John De Costa",
+	},
+	plugins: [
+		"gatsby-plugin-react-helmet",
+		{
+			resolve: "gatsby-source-filesystem",
+			options: {
+				name: "images",
+				// eslint-disable-next-line no-undef
+				path: `${__dirname}/src/images`,
+			},
+		},
+		"gatsby-transformer-sharp",
+		"gatsby-plugin-sharp",
+		{
+			resolve: "gatsby-plugin-manifest",
+			options: {
+				name: "",
+				short_name: "",
+				start_url: "/",
+				background_color: "#f5f5f5",
+				theme_color: "#f5f5f5",
+				display: "minimal-ui",
+				icon: "src/images/fox-icon.png", // This path is relative to the root of the site.
+			},
+		},
+		// this (optional) plugin enables Progressive Web App + Offline functionality
+		// To learn more, visit: https://gatsby.dev/offline
+		// 'gatsby-plugin-offline',
+	],
 }
--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,11 @@
-import { Link } from "gatsby"
+
 import PropTypes from "prop-types"
 import React from "react"
 
-const Header = ({ siteTitle }) => (
+const  Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
+      background: `whitesmoke`,
       marginBottom: `1.45rem`,
     }}
   >
@@ -16,16 +16,8 @@ const Header = ({ siteTitle }) => (
         padding: `1.45rem 1.0875rem`,
       }}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
+      <h1 style={{ margin: 0, textAlign: "center" }}>
+         Calculator
       </h1>
     </div>
   </header>
